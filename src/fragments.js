aedfb203d3a4e937843e98c813cf51eaa7be4e63
tonnac <tonnac35@gmail.com>
2019-12-31 18:05:15 +0900

    3.19 seeFullPost Refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -1,21 +1,33 @@
 export const USER_FRAGMENT = `
-fragment UserParts on User {
     id
-    username
-    email
-    firstName
-    lastName
-    bio
-    posts {
-        id
-        caption
-    }
-}`;
+    username 
+`;
 
-export const COMMENT_FRAGMENT = `fragment CommentParts on Commit{
+export const COMMENT_FRAGMENT = `
     id
     text
     user{
-        username
-    }
+        ${USER_FRAGMENT}
 }`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments{
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
